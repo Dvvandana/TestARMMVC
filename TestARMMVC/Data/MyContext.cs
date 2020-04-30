1bdc06360cbd36be6c6d1653f18bcc35df605bef
Vishnu Desu <dvvandana@gmail.com>
2020-04-30 16:19:02 -0700

    Add project files.

---
+++ TestARMMVC/Data/MyContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestARMMVC.Models;
+
+namespace TestARMMVC.Data
+{
+    public class MyContext : DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Student { get; set; }
+    
+    }
+}
---
+++ TestARMMVC/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestARMMVC.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string Name { get; set; }
+
+    }
+}
